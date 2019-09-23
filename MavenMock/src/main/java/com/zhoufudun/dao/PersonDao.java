2c919050ca32492d0f6f2270126948e7e7ba6385
zhoufudun <1215906004@qq.com>
2019-09-23 18:02:42 +0800

    mock

--- MavenMock/src/main/java/com/zhoufudun/dao/PersonDao.java
+++
@@ -1,9 +0,0 @@
-package com.zhoufudun.dao;
-
-import com.zhoufudun.entity.Person;
-
-public interface PersonDao {
-	public Person fetchPerson(Integer personID);  
-    public void update(Person person);  
-    public void insert(Person person);
-}
--- MavenMock/src/main/java/com/zhoufudun/quartz/demo/JobTwoDemo.java
+++
@@ -1,8 +0,0 @@
-package com.zhoufudun.quartz.demo;
-
-import java.util.Date;
-public class JobTwoDemo {
-	public void sayHello() {  
-        //System.out.println(new Date() + " -> Hello, 我是任务 2");  
-    } 
-}
